c16fbb4588a729a9fd982d96825f4001e491cee7
Robson Venturin <robventurin@gmail.com>
2020-05-17 09:39:31 -0300

    Alterado rotas

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', function () { return view('index'); });
 
 
-Route::get('/categoria/listar_todos', function () {
-    return view('listar_categoria');
-});
+Route::get('/categoria/listar_todos', function () { return view('listar_categoria'); });
 
-Route::get('/busca/resultado', function () {
-    return view('listar_busca');
-});
+Route::get('/busca/resultado', function () { return view('listar_busca'); });
 
-Route::get('/produto/1', function () {
-    return view('produto');
-});
+Route::get('/produto/1', function () { return view('produto'); });
 
-Route::get('/carrinho', function () {
-    return view('carrinho');
-});
+Route::get('/carrinho', function () { return view('carrinho'); });
+
+
+Route::get('/categoria_produtos/listar', function () { return view('categoria_produtos.listar'); });
+Route::get('/categoria_produtos/adicionar', function () { return view('categoria_produtos.adicionar'); });
+Route::get('/categoria_produtos/alterar/1', function () { return view('categoria_produtos.alterar'); });
+
+Route::get('/produtos/listar', function () { return view('produtos.listar'); });
+Route::get('/produtos/adicionar', function () { return view('produtos.adicionar'); });
+Route::get('/produtos/alterar/1', function () { return view('produtos.alterar'); });
+
+Route::get('/cidades/listar', function () { return view('cidades.listar'); });
+Route::get('/cidades/adicionar', function () { return view('cidades.adicionar'); });
+Route::get('/cidades/alterar/1', function () { return view('cidades.alterar'); });
+
+Route::get('/vendas/listar', function () { return view('vendas.listar'); });
+Route::get('/vendas/listar/itens/1', function () { return view('vendas.listar_itens'); });
+
+Route::get('/enderecos/listar', function () { return view('enderecos.listar'); });
+Route::get('/enderecos/adicionar', function () { return view('enderecos.adicionar'); });
+Route::get('/enderecos/alterar/1', function () { return view('enderecos.alterar'); });
 
 Auth::routes();
 
